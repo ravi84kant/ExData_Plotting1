81a545786bf97c1d3733c2680a6447da7a6e9c8a
ravi84kant <48993733+ravi84kant@users.noreply.github.com>
2019-05-11 12:42:19 +0530

    Delete Plot1.R

--- Plot1.R
+++
@@ -1,5 +0,0 @@
-# Code for Plot 1
-hist(as.numeric(house_sel_data$Global_active_power), col = "red", 
-     main = "Global Active Power", 
-     xlab = "Global Active power(kilowatts)", 
-     ylab = "Frequency")
